a49a98947d8ac9eac8f3b5ebe854ff8b268e95b1
mpascualach <miguelpacheson@hotmail.es>
2023-04-29 22:00:43 +0300

    actual first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Start from './Start.js'
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Start.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function Start() {
+    return (
+        <Button>
+            Start
+        </Button>
+    )
+}
